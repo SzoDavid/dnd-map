fce9929fcabbf4308d8fe76a2dad79f7cb5c264b
SzoDavid <szobonya.david@gmail.com>
2022-06-26 14:59:56 +0200

    created database

--- dnd_map/admin.py
+++ dnd_map/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Kingdom, City, Place
+
+
+class KingdomAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    ordering = ['name']
+
+    list_display = ('name', 'discovered')
+
+
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'type']
+    ordering = ['kingdom', 'name']
+
+    list_display = ('name', 'type', 'kingdom', 'discovered')
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'type']
+    ordering = ['city', 'name']
+
+    list_display = ('name', 'type', 'city', 'discovered')
+
+
+admin.site.register(Kingdom, KingdomAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Place, PlaceAdmin)
--- dnd_map/models.py
+++ dnd_map/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Kingdom(models.Model):
+    name = models.CharField(max_length=64)
+    pronunciation = models.CharField(max_length=64, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    map_path = models.CharField(max_length=32, blank=True, null=True)
+    coords = models.CharField(max_length=19, blank=True, null=True)
+    discovered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=64)
+    pronunciation = models.CharField(max_length=64, blank=True, null=True)
+    kingdom = models.ForeignKey(Kingdom, on_delete=models.CASCADE)
+    type = models.CharField(max_length=64, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    map_path = models.CharField(max_length=32, blank=True, null=True)
+    coords = models.CharField(max_length=19, blank=True, null=True)
+    discovered = models.BooleanField(default=False)
+    capital = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "cities"
+
+    def __str__(self):
+        return self.name
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=64)
+    pronunciation = models.CharField(max_length=64, blank=True, null=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    type = models.CharField(max_length=64, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    map_path = models.CharField(max_length=32, blank=True, null=True)
+    coords = models.CharField(max_length=19, blank=True, null=True)
+    discovered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
