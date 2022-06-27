f3edc8112a79e3573af8ffeb411793d658ed850d
SzoDavid <szobonya.david@gmail.com>
2022-06-27 23:32:30 +0200

    fix bugs

--- dnd_map/models.py
+++ dnd_map/models.py
@@ -20,7 +20,8 @@ class City(models.Model):
     type = models.CharField(max_length=64, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     map = models.ImageField(upload_to='maps/cities', blank=True, null=True)
-    coords = models.CharField(max_length=19, blank=True, null=True)
+    index_coords = models.CharField(max_length=19, blank=True, null=True)
+    kingdom_coords = models.CharField(max_length=19, blank=True, null=True)
     discovered = models.BooleanField(default=False)
     capital = models.BooleanField(default=False)
 
@@ -38,7 +39,9 @@ class Place(models.Model):
     type = models.CharField(max_length=64, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     map = models.ImageField(upload_to='maps/places', blank=True, null=True)
-    coords = models.CharField(max_length=19, blank=True, null=True)
+    index_coords = models.CharField(max_length=19, blank=True, null=True)
+    kingdom_coords = models.CharField(max_length=19, blank=True, null=True)
+    city_coords = models.CharField(max_length=19, blank=True, null=True)
     discovered = models.BooleanField(default=False)
 
     def __str__(self):
