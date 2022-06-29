1d7422e3ea4943a9bee29bbcc406487032dfc046
SzoDavid <szobonya.david@gmail.com>
2022-06-29 19:12:13 +0200

    list view frontend

---
+++ dnd_map/static/dnd_map/scripts/generateListView.js
@@ -0,0 +1,69 @@
+const admin = '<a class="toggle{0}" href="{1}">{2}</a><a class="toggle{3}" href="{4}">{5}</a><a class="button" href="{6}">✎</a>'
+const add = '<a class="button" href="{0}">+</a>'
+const div = '<div class="list_column">{0}<a href="{1}">{2}</a>{3}</div>{4}'
+const li = '<li>{0}<a href="{1}">{2}</a>{3}</li>{4}'
+
+String.prototype.format = function () {
+    // store arguments in an array
+    let args = arguments;
+    // use replace to iterate over the string
+    // select the match and check if related argument is present
+    // if yes, replace the match with the argument
+    return this.replace(/{(\d+)}/g, function (match, index) {
+        // check if the argument is present
+        return typeof args[index] == 'undefined' ? match : args[index];
+    });
+};
+
+function generateList(items, auth) {
+    let result = ''
+
+    items.forEach(function (item) {
+        result += li.format(
+            auth ? admin.format(
+                !item['discovered'] ? ' off' : '',
+                item['toggle_discovered'],
+                item['discovered'] ? '✓' : '✗',
+                !item['description'] ? ' off' : '',
+                item['toggle_description'],
+                item['description'] ? '✓' : '✗',
+                item['edit']
+            ) : '',
+            item['details'],
+            item['name'],
+            item['children'].length === 0 ? '' : `<ul>${generateList(item["children"], auth)}</ul>`,
+            auth ? `<li>${add.format(item["add_child"])}</li>` : ''
+        )
+    })
+
+    return result
+}
+
+function populateListView(json, auth, add_div_url) {
+    const items = JSON.parse(json)
+
+    console.log(items)
+
+    let result = ''
+
+    items.forEach(function (item) {
+        result += div.format(
+            auth ? admin.format(
+                !item['discovered'] ? ' off' : '',
+                item['toggle_discovered'],
+                item['discovered'] ? '✓' : '✗',
+                !item['description'] ? ' off' : '',
+                item['toggle_description'],
+                item['description'] ? '✓' : '✗',
+                item['edit']
+            ) : '',
+            item['details'],
+            item['name'],
+            item['children'].length === 0 ? '' : `<ul>${generateList(item['children'], auth)}</ul>`,
+            auth ? `<div class="list_column">${add.format(add_div_url)}</div>` : ''
+        )
+    })
+
+    document.getElementById('list_view').innerHTML = result
+}
+
