ca4b3ec2638f636015a5824459f3bd1212c40052
SzoDavid <szobonya.david@gmail.com>
2022-07-07 01:19:32 +0200

    Make list view collapsable

--- dnd_map/static/dnd_map/scripts/generateListView.js
+++ dnd_map/static/dnd_map/scripts/generateListView.js
@@ -1,61 +1,72 @@
-const admin = '<tr><td><a class="toggle{0}" href="{1}">⚑</a><a class="toggle{2}" href="{3}">☰</a><a class="button" ' +
-    'href="{4}">✎</a><a class="button" href="{5}">Add</a></td></tr>'
-const div = '<div class="list_column"><table class="item"><tr><td><a href="{0}">{1}</a></td></tr>{2}</table><ul>{3}</ul></div>'
-const li = '<li><table class="item"><tr><td><a href="{0}">{1}</a></td></tr>{2}</table><ul>{3}</ul></li>'
-
 let max_depth = null
 
-function generateList(items, auth) {
-    let result = ''
+function generateItem(item, auth) {
+    let table = document.createElement('table')
+    table.classList.add('item')
 
-    items.forEach(function (item) {
-        result += li.format(
-            item['details'],
-            `<strong>${item['name']}</strong><br><em>${item['type']}</em>`,
-            auth ? admin.format(
-                !item['discovered'] ? ' off' : '',
-                item['toggle_discovered'],
-                !item['description'] ? ' off' : '',
-                item['toggle_description'],
-                item['edit'],
-                item['add_child']
-            ) : '',
-            item['children'].length === 0 ? '' : `${generateList(item['children'], auth)}`,
-        )
-    })
+    let tr_title = document.createElement('tr')
+    tr_title.innerHTML = `<td><a href="${item['details']}"><strong>${item['name']}</strong><br><em>${item['type']}</em></a></td>`
 
-    return result
+    table.append(tr_title)
+
+    if (item['children'].length !== 0) {
+        tr_title.innerHTML += `<td rowspan="2"><button class="toggle" onclick="collapse(this)">⮟</button></td>`
+    }
+
+    if (auth) {
+        table.innerHTML += `<tr><td><a class="toggle${item['discovered'] ? '' : ' off'}" ` +
+            `href="${item['toggle_discovered']}">⚑</a><a class="toggle${item['description'] ? '' : ' off'}" ` +
+            `href="${item['toggle_description']}">☰</a><a class="button" href="${item['edit']}">✎</a><a ` +
+            `class="button" href="${item['add_child']}">Add</a></td></tr>`
+    }
+
+    if (item['children'].length !== 0) {
+        let ul_children = document.createElement('ul')
+        table.append(ul_children)
+
+        item['children'].forEach(function (child) {
+            let child_item = generateItem(child, auth)
+            let li_child = document.createElement('li')
+            li_child.append(child_item)
+            ul_children.append(li_child)
+        })
+    }
+
+    return table
+}
+
+function collapse(obj) {
+    let button = obj
+    let table = obj.parentNode.parentNode.parentNode.parentNode
+    button.classList.toggle('off')
+    if (button.textContent === '⮟') {
+        table.getElementsByTagName('ul')[0].hidden = true
+        button.textContent = '⮜'
+    } else {
+        table.getElementsByTagName('ul')[0].hidden = false
+        button.textContent = '⮟'
+    }
 }
 
 function populateListView(json, auth, add_div_url) {
+    const div = document.getElementById('list_view')
     const items = JSON.parse(json)
 
-    let result = ''
-
     items.forEach(function (item) {
-        if (max_depth == null) {
+        if (max_depth === null) {
             max_depth = item['max_depth']
         } else {
-            result += div.format(
-                item['details'],
-                `<strong>${item['name']}</strong><br><em>${item['type']}</em>`,
-                auth ? admin.format(
-                    !item['discovered'] ? ' off' : '',
-                    item['toggle_discovered'],
-                    !item['description'] ? ' off' : '',
-                    item['toggle_description'],
-                    item['edit'],
-                    item['add_child'],
-                ) : '',
-                item['children'].length === 0 ? '' : `${generateList(item['children'], auth)}`,
-            )
+            let div_item = document.createElement('div')
+            div_item.classList.add('list_column')
+            div_item.append(generateItem(item, auth))
+            div.append(div_item)
         }
     })
 
     if (auth) {
-        result += `<div class="list_column"><a class="button" href="${add_div_url}">+</a></div>`
+        let div_append = document.createElement('div')
+        div_append.classList.add('list_column')
+        div_append.innerHTML = `<a class="button" href="${add_div_url}">+</a>`
+        div.append(div_append)
     }
-
-    document.getElementById('list_view').innerHTML = result
 }
-
