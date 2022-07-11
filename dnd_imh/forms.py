7b1679714268cf4df55d28cd616cb7eeb6ff0b76
SzoDavid <szobonya.david@gmail.com>
2022-07-11 21:59:55 +0200

    Validate world form

--- dnd_imh/forms.py
+++ dnd_imh/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.utils.safestring import mark_safe
 
 from dnd_imh.models import World
@@ -16,6 +17,18 @@ class WorldForm(forms.ModelForm):
                                                      'tree. Smaller than or equal to '
                                                      '<em>max item child tree depth</em>')
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        max_item_tree_depth = cleaned_data.get('max_item_tree_depth')
+        max_item_tree_display_depth = cleaned_data.get('max_item_tree_display_depth')
+
+        if max_item_tree_depth and max_item_tree_display_depth:
+            if max_item_tree_depth <= 0:
+                raise ValidationError('Max item tree depth must be larger than 0!', code='invalid')
+            if max_item_tree_display_depth <= 0 or max_item_tree_display_depth > max_item_tree_depth:
+                raise ValidationError('Max item tree display depth must be larger than 0 and smaller than or equal to '
+                                      'max item child tree depth!', code='invalid')
 
 
 class RegisterForm(UserCreationForm):
