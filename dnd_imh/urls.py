5a990db800d65a65ab83a25639dda1b60bda4528
Szobonya Dávid <46794488+SzoDavid@users.noreply.github.com>
2022-07-15 23:31:09 +0200

    Added user page (#24)
    * Added user page
    
    * Added user links to worlds page
    
    * Fixed page titles
    
    * Drop down menu on the menu bar
    
    * Added user icon and adjusted style
    
    * Made user-icon white
    
    * Improved menubar and css for dnd_imh
    
    * Updated README.md

--- dnd_imh/urls.py
+++ dnd_imh/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('worlds', views.worlds, name='worlds'),
     path('createworld', views.create_world, name='create_world'),
     path('<int:world_pk>/editworld', views.edit_world, name='edit_world'),
-    path('<int:world_pk>/removeworld', views.remove_world, name='remove_world')
+    path('<int:world_pk>/removeworld', views.remove_world, name='remove_world'),
+    path('user/<int:user_pk>', views.user, name='user')
 ]
